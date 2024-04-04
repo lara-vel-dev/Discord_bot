9f6c666148fc693e741e23b424bd6a502a16e0d7
Daniela Lara <laradni@icloud.com>
2024-04-04 12:25:20 -0600

    Add event when user joins

--- main.py
+++ main.py
@@ -1,26 +1,57 @@
 # Modules
 import os
-from discord import Intents, Client, Message
+from discord import Intents, Member, File
 from discord.ext import commands
 from dotenv import load_dotenv
+from easy_pil import Editor, load_image_async, Font
 from typing import Final
 
-# Import discord token
 load_dotenv()
+
+# Import discord token
 TOKEN: Final[str] = os.getenv("DISCORD_TOKEN")
+
+# Import id's
+WELCOME_ID: Final[int] = int(os.getenv("WELCOME_CHANNEL_ID"))
+GOODBYE_ID: Final[int] = int(os.getenv("FAREWELL_CHANNEL_ID"))
 
 # Command identifier
 bot = commands.Bot(command_prefix='!', intents=Intents.all())
+
+# --- Bot events --- #
 
 # When the bot is ready to use
 @bot.event
 async def on_ready():
     print("I'm ready!!")
 
+# Sends message when a user joins the server
+@bot.event
+async def on_member_join(member: Member):
+    channel = member.guild.system_channel
+    backgroung = Editor("1.png")
+    profile_image = await load_image_async(str(member.avatar.url))
+    profile = Editor(profile_image).resize((290, 290)).circle_image()
+    poppins = Font.poppins(size=25, variant="bold")
+
+    backgroung.paste(profile, (380, 155))
+    backgroung.text((525, 475), f"{member.name}#{member.discriminator}",
+                    color="white", font=poppins, align="center")
+    backgroung.ellipse((380, 155), 290, 290, outline="white", stroke_width=5)
+
+    file = File(fp=backgroung.image_bytes, filename="1.png")
+    await channel.send(f"Hello {member.mention}! Welcome to Danielandia")
+    await channel.send(file=file)
+
+# Sends message when a user leaves the server
+@bot.event
+async def on_member_remove(member):
+    channel = bot.get_channel(GOODBYE_ID)
+    await channel.send("Goodbye!")
 
 # --- Basic commands --- #
 
-# Greet the user
+# Greet the users
 @bot.command()
 async def greetings(ctx):
     await ctx.send("Hello there!")
@@ -32,7 +63,6 @@ async def farewell(ctx):
     await ctx.send("See you soon!")
 
 
-# Init Bot
-bot.run(TOKEN)
-
-    
+if __name__ == "__main__":
+    # Init Bot
+    bot.run(TOKEN)
